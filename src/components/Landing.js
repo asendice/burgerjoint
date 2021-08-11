0628b02f3cb2f7f3843fe397003f5a4cb054c7c1
Dylan Travis <dylan.travis23@outlook.com>
2021-08-10 18:04:22 -0700

    navigation bar hamburger responsive

--- src/components/Landing.js
+++ src/components/Landing.js
@@ -1,13 +1,12 @@
 import React from "react";
-import landing from "../img/landscape.jpeg";
 
 const Landing = () => {
   return (
     <div className="landing">
-      <img src={landing} />
       <div className="title">
-        <h1>MOUTH WATERING TASTE</h1>
-        <div></div>
+        <h1>MOUTH </h1>
+        <h1> WATERING </h1>
+        <h1> TASTE</h1>
         <div className="locations-btn">
           <p>Locations</p>
         </div>
--- src/components/NavBar.js
+++ src/components/NavBar.js
@@ -1,22 +1,54 @@
-import React from "react";
+import React, { useState } from "react";
 import bji from "../img/bji.png";
 
 const NavBar = () => {
+  const [showMenu, setShowMenu] = useState(false);
+
+  const toggleNavMenu = () => {
+    setShowMenu(!showMenu);
+  };
+
   return (
-    <div className="navbar">
-      <div className="navbar-container">
-        <div>Menu</div>
-        <div>Shakes & Cocktails</div>
-        <div>Locations</div>
+    <>
+      <div className="navbar">
         <div className="bji-icon-container">
           <img className="bji-icon" src={bji} />
           <p>Burger Joint</p>
         </div>
-        <div>Join BurgerJoint?</div>
-        <div>Contact Us</div>
-        <div>Order Now</div>
+        <ul
+          className="nav-links"
+          style={{
+            display: `${showMenu ? "none" : "flex"}`,
+          }}
+        >
+          <li>
+            <a>Menu</a>
+          </li>
+          <li>
+            <a>Beverages</a>
+          </li>
+          <li>
+            <a>Locations</a>
+          </li>
+          <li>
+            <a>Member ?</a>
+          </li>
+          <li>
+            <a>Contact Us</a>
+          </li>
+          <li>
+            <a>Order Now</a>
+          </li>
+        </ul>
+        <div className="nav-burger-container">
+          <div onClick={() => toggleNavMenu()} className="nav-burger">
+            <div className="line-1"></div>
+            <div className="line-2"></div>
+            <div className="line-3"></div>
+          </div>
+        </div>
       </div>
-    </div>
+    </>
   );
 };
 
