46085dd423cfdb2aea2b7034184d70b89753bbe2
Dylan Travis <dylan.travis23@outlook.com>
2021-08-19 13:40:34 -0700

    removed test code

--- models/Menu.js
+++ models/Menu.js
@@ -3,14 +3,6 @@ const Schema = mongoose.Schema;
 
 const menuSchema = new Schema(
   {
-    // name: {
-    //   type: String,
-    //   required: true,
-    // },
-    // price: {
-    //   type: String,
-    //   required: true,
-    // },
     category: {
       type: String,
       required: true,
